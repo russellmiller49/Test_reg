d3b2e31f9deaade9cc5132e37c7d20cf8d082a56
Russellmiller <russellmiller49@gmail.com>
2025-10-05 15:00:11 -0700

    Initial Commit

---
+++ Bronch_registry/Bronch_registryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bronch_registryApp.swift
+//  Bronch_registry
+//
+//  Created by Russell Miller on 10/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Bronch_registryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bronch_registry/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bronch_registry
+//
+//  Created by Russell Miller on 10/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
